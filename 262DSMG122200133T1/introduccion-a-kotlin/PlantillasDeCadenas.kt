ab6e22095e794411828544410b641eeea1d763de
Vilval25 <138557947+Vilval25@users.noreply.github.com>
2026-09-01 01:45:30 -0500

    Create PlantillasDeCadenas.kt

---
+++ 262DSMG122200133T1/introduccion-a-kotlin/PlantillasDeCadenas.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val discountPercentage = 20
+    val offer: String
+    val item = "Google Chromecast"
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    
+    println(offer)
+}
